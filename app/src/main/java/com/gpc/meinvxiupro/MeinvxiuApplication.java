5d3918e9fd04ff6e1a0933b80139132ede0ef472
Gupengcheng <370927170@qq.com>
2016-03-01 16:39:07 +0800

    [v1.0] init project packages

---
+++ app/src/main/java/com/gpc/meinvxiupro/MeinvxiuApplication.java
@@ -0,0 +1,13 @@
+package com.gpc.meinvxiupro;
+
+import android.app.Application;
+
+/**
+ * Created by pcgu on 16-3-1.
+ */
+public class MeinvxiuApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}

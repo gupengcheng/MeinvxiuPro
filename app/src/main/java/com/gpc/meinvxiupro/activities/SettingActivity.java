7a3280b43ecd9c3e2937393826bd936e09f75ce4
Gupengcheng <370927170@qq.com>
2016-04-22 18:18:19 +0800

    添加设置

--- app/src/main/java/com/gpc/meinvxiupro/activities/SettingActivity.java
+++ app/src/main/java/com/gpc/meinvxiupro/activities/SettingActivity.java
@@ -1,16 +1,45 @@
 package com.gpc.meinvxiupro.activities;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 
 import com.gpc.meinvxiupro.R;
+import com.gpc.meinvxiupro.models.SettingItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by pcgu on 16-4-22.
  */
 public class SettingActivity extends BaseActivity {
+    private RecyclerView mSettingRecyclerView;
+    private List<SettingItem> mDatas;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+    }
+
+    @Override
+    protected void findViewByIds() {
+        super.findViewByIds();
+        mSettingRecyclerView = (RecyclerView) findViewById(R.id.setting_recycler_view);
+    }
+
+    @Override
+    protected void initViews() {
+        super.initViews();
+        getDatas();
+    }
+
+    private void getDatas() {
+        mDatas = new ArrayList<>();
+        String[] settingKey = getResources().getStringArray(R.array.page_style_key);
+        String[] settingContent = getResources().getStringArray(R.array.page_style_content);
+        for (int i = 0; i < Math.min(settingKey.length, settingContent.length); i++) {
+
+        }
     }
 }
---
+++ app/src/main/java/com/gpc/meinvxiupro/models/SettingItem.java
@@ -0,0 +1,25 @@
+package com.gpc.meinvxiupro.models;
+
+/**
+ * Created by pcgu on 16-4-22.
+ */
+public class SettingItem {
+    private String mSettingContent;
+    private String mSettingKey;
+
+    public String getSettingContent() {
+        return mSettingContent;
+    }
+
+    public void setSettingContent(String mSettingContent) {
+        this.mSettingContent = mSettingContent;
+    }
+
+    public String getSettingKey() {
+        return mSettingKey;
+    }
+
+    public void setSettingKey(String mSettingKey) {
+        this.mSettingKey = mSettingKey;
+    }
+}
